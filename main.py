732eabe7f1fcfc5d1cc134931cc77fdafb06ae63
BuildTools <unconfigured@null.spigotmc.org>
2022-02-28 13:10:36 +0100

    Search for tweets (doesn't work yet)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from textblob import TextBlob
 import tweepy
 import sys
 
-#Auth
+# Auth
 api_key = 'FMKyMEo9O9kG9nk7VHfGR1pli'
 api_key_secret = 'oNKdRioupeAJRYlH3fMUihjB8lgfYiVEyyNceHJAYKeDPtLJ9b'
 access_token = '1294624994287329280-0MlJBIGchblHVG266yiTpPTyVU2D0t'
@@ -12,18 +12,21 @@ auth_handler = tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key
 auth_handler.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth_handler)
 
-#Keywords
-keyword = input("Please enter a keyword or hashtag:")
-nrOfTweets = input("Please enter how many tweets to analyze:")
+# Keywords
+keyword = 'tweepy'#input("Please enter a keyword or hashtag:")
+nrOfTweets = 10 #input("Please enter how many tweets to analyze:")
+
 
 def check_int(nrOfTweets):
     try:
         val = int(nrOfTweets)
     except ValueError:
-        print("nope")
+        print("This is not a number")
+
 
 check_int(nrOfTweets)
-#def percentage
- #   return 100 * float(part)/float(whole)
 
+tweets = tweepy.cursor(api.search_tweets, q=keyword, lang = 'en').items(nrOfTweets)
 
+for tweet in tweets:
+   print(tweet.text)
